49dfc21d250318896e929a1e6f25215edcf587c1
asmitasharma16 <asmitaas73@gmail.com>
2018-08-17 19:47:21 +0530

    Introduction to Python

---
+++ Assignment2.py
@@ -0,0 +1,30 @@
+#1.
+print('We are learning Python')
+#2
+s1="HELLO "
+s2="GIT"
+s3=s1+s2 #concatinate s1 and s2
+print(s3)
+print("Acad"+"View")
+#3
+x=int(input("x = "))
+y=int(input("y = "))
+z=int(input("z = "))
+print(x,y,z)
+#4
+print('"Let’s get started"')
+#5
+s="Acadview"
+course="Python"
+fees=5000
+print('s= %s'%(s))
+print('course= %s'%(course))
+print('fees= %d'%(fees))
+#6
+r = int(input("enter radius of circle = "))
+pi=3.14
+a =pi*r*r
+print("area of circle is = ",a)
+
+
+
